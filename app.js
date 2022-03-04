4b33671014058cd2cefd3dd412d697b7a946a5d1
Zuhaib <67638966+powrhouseofthecell@users.noreply.github.com>
2022-03-04 08:31:36 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 
 const PORT = process.env.PORT || 3000;
-const uri = process.env.URI
+const uri = process.env.uri
 
 const app = express();
 
